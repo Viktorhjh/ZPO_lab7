5bd2bb25b0e5c7a7dae9687c02c18e4b07f076bd
Viktorhjh <Viktorhjh@VIKTORHJH>
2022-05-20 01:18:22 +0200

    Add project files.

---
+++ ZPO_lab7/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZPO_lab7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Person> list = new List<Person>();        
+        public MainWindow()
+        {
+            InitializeComponent();
+            list.Add(new Person("qwe","asd",54, "zxc"));
+            list.Add(new Person("rty", "fgh", 98, "vbn"));
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                list.Add(new Person(textBoxName.Text, textBoxSurname.Text, int.Parse(textBoxYear.Text), textBoxCity.Text));
+            }
+            catch
+            {
+                MessageBox.Show("Error!!");
+            }
+
+            listView.ItemsSource = list;
+            listView.Items.Refresh();
+        }
+    }
+}
---
+++ ZPO_lab7/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZPO_lab7
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+        public Person(string name, string surname, int year, string city)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = year;
+            this.City = city;
+        }
+
+    }
+}
